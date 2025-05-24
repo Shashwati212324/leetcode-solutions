9ea685d8091659f5eca7fde786431e2cb4e4374e
Shashwati Jha <141508757+Shashwati212324@users.noreply.github.com>
2025-05-24 18:22:37 +0530

    Time: 2 ms (74.86%), Space: 53 MB (65.79%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,83 +1,64 @@
 public class Solution {
-    // public static void main(String[] args) {
-    //     // Warm-up phase
-    //     for (int i = 0; i < 300; i++) {
-    //         majorityElement(new int[] {1, 2, 2, 3, 2, 2, 4});
-    //     }
-
-    // }//     // Benchmarking phase
-    //     long startTime = System.nanoTime();
-    //     System.out.println(majorityElement(new int[] {1, 2, 2, 3, 2, 2, 4}));
-    //     long endTime = System.nanoTime();
-
-    //     System.out.println("Execution Time: " + (endTime - startTime) + " ns");
-    // }
-
-    public static int majorityElement(int[] nums) {
-        int result =0;
-       
-        int major = 0;
-        int j =0;
-        if(nums.length == 1){
-            return nums[0];
-        }
-        while(j<nums.length-1){
-            int count =1;
-            int temp = nums[j];
-            if(temp != major){
-                for(int i=j+1;i<nums.length;i++){
-                    if(nums[i]==temp){
-                        count++;
-                    }
-                    if(result<count){
-                        result=count;
-                        if(result>=nums.length/2){
-                            major = temp;
-                        }
-                    }
-                    
+    public static int majorityElement(int[] nums) {  
+// APPROACH - 1 - WITHOUT SRTING, USING LOOP
+        // if(nums.length == 1){
+        //     return nums[0];
+        // }    
+        // for(int j=0;j<nums.length;j++){
+        //     int count =1;
             
-                }
-            }
-            j++;
-        }
-        return major;
-
-
-
-
-
-
-
-
-    //     for(int i=0; i<nums.length-1; i++){
-    //         for(int j =i+1; j>0;j--){
-    //             if(nums[j]<nums[j-1]){
-    //                 int temp = nums[j];
-    //                 nums[j] = nums[j-1];
-    //                 nums[j-1]= temp;
-    //             }
-    //         }
-    //     }
-    //     int count =1;
-    //     int finalee = nums[0];
-    //     for(int i =0; i<nums.length-1 ;i++){
-    //         if(nums[i]==nums[i+1]){
-    //             count++;
-    //             if(count>nums.length/2){
-    //                 finalee= nums[i];
-    //             }
-    //         }
-    //         else{
-    //             count = 1;
-    //            // finalee = nums[i];
-    //         }
+        //         for(int i=j+1;i<nums.length;i++){
+        //             if(nums[i]==nums[j]){
+        //                 count++;
+        //             }
+        //             if(count>nums.length/2){
+                        
+        //                 return nums[j];
+        //             }
+        //         }
             
-    //     }
-    //     return finalee;
-    // }
-    // public int majority(int[] arr){
-        
+            
+        // }
+        // return -1;
+
+// APPROACH - 2 - sorting the array
+
+        // for(int i=0; i<nums.length-1; i++){
+        //     for(int j =i+1; j>0;j--){
+        //         if(nums[j]<nums[j-1]){
+        //             int temp = nums[j];
+        //             nums[j] = nums[j-1];
+        //             nums[j-1]= temp;
+        //         }
+        //     }
+        // }
+        // int count =1;
+        // int finalee = nums[0];
+        // for(int i =0; i<nums.length-1 ;i++){
+        //     if(nums[i]==nums[i+1]){
+        //         count++;
+        //         if(count>nums.length/2){
+        //             finalee= nums[i];
+        //         }
+        //     }
+        //     else{
+        //         count = 1;
+        //     }
+            
+        // }
+        // return finalee; 
+// APPROACH - 3 - MOORE VOTING ALGORITHM
+    int count = 0;
+    int candidate = 0;
+
+    for (int num : nums) {
+        if (count == 0) {
+            candidate = num;
+        }
+        count += (num == candidate) ? 1 : -1;
+    }
+
+    return candidate;           
         
     }
 }
