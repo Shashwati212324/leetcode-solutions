5ce6aa4b7ed873061c421fcb1ac10f9c0502f57e
Shashwati Jha <141508757+Shashwati212324@users.noreply.github.com>
2025-08-24 11:50:03 +0530

    Time: 3 ms (91.69%), Space: 43.2 MB (86.14%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -1,26 +1,50 @@
 
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        Map<Integer, Integer> nextGreater = new HashMap<>();
         Stack<Integer> stack = new Stack<>();
-
-        for (int num : nums2) {
-            while (!stack.isEmpty() && stack.peek() < num) {
-                nextGreater.put(stack.pop(), num);
-            }
-            stack.push(num);
-        }
-
-        // Remaining elements in stack don't have any next greater
-        while (!stack.isEmpty()) {
-            nextGreater.put(stack.pop(), -1);
-        }
-
         int[] result = new int[nums1.length];
-        for (int i = 0; i < nums1.length; i++) {
-            result[i] = nextGreater.get(nums1[i]);
+        for (int i =0; i<nums1.length ; i++) {
+            int val = nums1[i];
+            boolean found = false;
+            int index = findIndex(val, nums2);
+            if(index < nums2.length-1){
+                for (int j = index + 1; j < nums2.length; j++) {
+                    if (nums2[j] > val) {
+                        stack.push(nums2[j]);
+                        found=true;
+                        break;
+                    }
+                }    
+            }
+            if(!found){
+                stack.push(-1);
+            }           
+            
         }
-
-        return result;
+        
+        
+        for (int i = 0; i < nums1.length; i++) {
+            result[i] = stack.pop();
+        }
+        return(reverse(result));
+    }
+    public int findIndex(int val, int[] nums){
+        for(int i =0; i<nums.length ; i++ ){
+            if(nums[i] == val){
+                return i;
+            }
+        }
+        return -1;
+    }
+    public int[] reverse(int[] nums){
+        int i =0, e = nums.length-1; 
+        while(i<e){
+            int temp =nums[i];
+            nums[i]=nums[e];
+            nums[e] =temp;
+            i++;
+            e--;
+        }
+        return nums;
     }
 }
