64742b7d04bf93b92f5e7070b45cbd56616857f1
Shashwati Jha <141508757+Shashwati212324@users.noreply.github.com>
2025-10-31 16:36:42 +0530

    Time: 2 ms (63.09%), Space: 46.5 MB (7.71%) - LeetHub

---
+++ 3581-the-two-sneaky-numbers-of-digitville/3581-the-two-sneaky-numbers-of-digitville.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Solution {
+    public int[] getSneakyNumbers(int[] nums) {
+        Set<Integer> seen = new HashSet<>();
+        List<Integer> duplicates = new ArrayList<>();
+
+        for (int num : nums) {
+            if (seen.contains(num)) {
+                duplicates.add(num);
+            } else {
+                seen.add(num);
+            }
+        }
+
+        return new int[]{duplicates.get(0), duplicates.get(1)};
+    }
+}
