905fd4cbb1aec292472bb47220fda7213eb9171d
Shashwati Jha <141508757+Shashwati212324@users.noreply.github.com>
2025-12-15 13:14:13 +0530

    Time: 2 ms (95.49%), Space: 45 MB (22.02%) - LeetHub

--- 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.java
+++ 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.java
@@ -1,30 +1,41 @@
 class Solution {
     public boolean checkIfExist(int[] arr) {
-        Arrays.sort(arr);
-        
-        for(int i =0; i<arr.length; i++){
-            int target = 2*arr[i];
-            if( BS(arr, target, i) == true){
+        //----------------------Hashset-
+        HashSet<Integer> set = new HashSet<>();
+        for(int i =0; i<arr.length ; i++){
+            if(set.contains(arr[i]*2) || (arr[i]%2==0 &&set.contains(arr[i]/2))){
                 return true;
-            } 
+            }
+            set.add(arr[i]);
         }
         return false;
+        //--------------------------6ms------------------------
+    //     Arrays.sort(arr);
         
-    } 
-    public static boolean BS(int[] arr, int target, int index){
-        int s =0; int e = arr.length-1;
-            while(s<=e){
-                int mid = s+(e-s)/2;
-                if(arr[mid] == target && mid != index){
-                    return true;
-                }
-                else if(arr[mid]>target){
-                    e= mid-1;
-                }
-                else{
-                    s = mid+1;
-                }
-            }
-            return false;
+    //     for(int i =0; i<arr.length; i++){
+    //         int target = 2*arr[i];
+    //         if( BS(arr, target, i) == true){
+    //             return true;
+    //         } 
+    //     }
+    //     return false;
+        
+    // } 
+    // public static boolean BS(int[] arr, int target, int index){
+    //     int s =0; int e = arr.length-1;
+    //         while(s<=e){
+    //             int mid = s+(e-s)/2;
+    //             if(arr[mid] == target && mid != index){
+    //                 return true;
+    //             }
+    //             else if(arr[mid]>target){
+    //                 e= mid-1;
+    //             }
+    //             else{
+    //                 s = mid+1;
+    //             }
+    //         }
+    //         return false;
+             
     }   
 }
