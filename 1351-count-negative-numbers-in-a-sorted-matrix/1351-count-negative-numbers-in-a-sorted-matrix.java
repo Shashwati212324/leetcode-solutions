745c81ada6adf6383b31905220c37ec299d7d072
Shashwati Jha <141508757+Shashwati212324@users.noreply.github.com>
2025-12-28 10:16:02 +0530

    Time: 0 ms (100%), Space: 47.3 MB (7.96%) - LeetHub

--- 1351-count-negative-numbers-in-a-sorted-matrix/1351-count-negative-numbers-in-a-sorted-matrix.java
+++ 1351-count-negative-numbers-in-a-sorted-matrix/1351-count-negative-numbers-in-a-sorted-matrix.java
@@ -3,25 +3,25 @@ class Solution {
         
         int count= 0;
         //Approach 1 - normal loop
-        // for(int r=0; r<grid.length;r++){
-        //     int s = 0; int e = grid[r].length-1;
-        //     while(s<=e){
-        //         int mid = s+(e-s)/2;
-        //    f     if(grid[r][mid]<0){e = mid-1;}
-        //         else{s=mid+1;}
-        //     }
-        //     count += (grid[r].length)-s;
+        for(int r=0; r<grid.length;r++){
+            int s = 0; int e = grid[r].length-1;
+            while(s<=e){
+                int mid = s+(e-s)/2;
+               if(grid[r][mid]<0){e = mid-1;}
+                else{s=mid+1;}
+            }
+            count += (grid[r].length)-s;
             
-        // }
+        }
         //approach 2- optimized loop
 
-        for(int i =0; i<grid.length; i++){
-            int l = grid[i].length;
-            for(int j =0; j<grid[0].length; j++ ){
-                if(grid[i][j]<0)count++;
-                if(grid[i][0]>0 && grid[i][l-1]>0)break;
-            }
-        }
+        // for(int i =0; i<grid.length; i++){
+        //     int l = grid[i].length;
+        //     for(int j =0; j<grid[0].length; j++ ){
+        //         if(grid[i][j]<0)count++;
+        //         if(grid[i][0]>0 && grid[i][l-1]>0)break;
+        //     }
+        // }
         
         return count;
         
