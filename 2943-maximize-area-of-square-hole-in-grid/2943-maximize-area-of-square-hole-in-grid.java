94e18c9fbb1d1cf59547a0f9bd46b9a95e1e9a94
Shashwati Jha <141508757+Shashwati212324@users.noreply.github.com>
2026-01-15 20:28:28 +0530

    Time: 5 ms (60.29%), Space: 45.5 MB (26.47%) - LeetHub

---
+++ 2943-maximize-area-of-square-hole-in-grid/2943-maximize-area-of-square-hole-in-grid.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class Solution {
+    public int maximizeSquareHoleArea(int n, int m, int[] hBars, int[] vBars) {
+        int maxH = longestConsecutive(hBars);
+        int maxV = longestConsecutive(vBars);
+
+        int side = Math.min(maxH, maxV);
+        return side * side;
+    }
+
+    private int longestConsecutive(int[] bars) {
+        Arrays.sort(bars);
+        int longest = 1, current = 1;
+
+        for (int i = 1; i < bars.length; i++) {
+            if (bars[i] == bars[i - 1] + 1) {
+                current++;
+                longest = Math.max(longest, current);
+            } else {
+                current = 1;
+            }
+        }
+        return longest + 1;
+    }
+}
