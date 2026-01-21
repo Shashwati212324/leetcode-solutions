4d68d19c7b2949a49d6adb281fbc9ff429ab7e06
Shashwati Jha <141508757+Shashwati212324@users.noreply.github.com>
2026-01-21 14:34:52 +0530

    Time: 3 ms (15.31%), Space: 43.6 MB (16.28%) - LeetHub

--- 1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.java
+++ 1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.java
@@ -1,21 +1,45 @@
 class Solution {
     public int countStudents(int[] students, int[] sandwiches) {
 
-        int[] count = new int[2];
+        //int[] count = new int[2];
 
-        // Count students' preferences
-        for (int s : students) {
-            count[s]++;
+        // // Count students' preferences
+        // for (int s : students) {
+        //     count[s]++;
+        // }
+
+        // // Process sandwiches
+        // for (int s : sandwiches) {
+        //     if (count[s] == 0) {
+        //         break;   // no student wants this sandwich
+        //     }
+        //     count[s]--;
+        // }
+
+        // return count[0] + count[1];
+
+        //Stack+Queue
+
+        Queue<Integer> q = new LinkedList<>();
+        Stack<Integer> stack= new Stack<>();
+
+        for(int s:students){
+            q.offer(s);
         }
-
-        // Process sandwiches
-        for (int s : sandwiches) {
-            if (count[s] == 0) {
-                break;   // no student wants this sandwich
+        for (int i = sandwiches.length - 1; i >= 0; i--) {
+    stack.push(sandwiches[i]);
+}
+        int rot=0;
+        while(!q.isEmpty()&&rot<q.size()){
+            if(q.peek().equals(stack.peek())){
+                q.poll();
+                stack.pop();
+                rot=0;
+            }else{
+                q.offer(q.poll());
+                rot++;
             }
-            count[s]--;
         }
-
-        return count[0] + count[1];
+        return q.size();
     }
 }
