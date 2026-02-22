98af60834b8a1ee6bb75d2cb8f218886eb8e8644
Shashwati Jha <141508757+Shashwati212324@users.noreply.github.com>
2026-02-22 20:09:38 +0530

    Time: 0 ms (100%), Space: 42.4 MB (48.75%) - LeetHub

---
+++ 0868-binary-gap/0868-binary-gap.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int binaryGap(int n) {
+        int temp =-1;
+        int gap =0;
+        int count =0;
+        while(n>0){
+            if((n&1)==1){
+                if (temp != -1) {
+                    gap = Math.max(gap, count-temp);
+                }
+                temp = count;
+            }
+            count++;
+            n=n>>1;
+        }    
+        return gap;
+    }
+}
