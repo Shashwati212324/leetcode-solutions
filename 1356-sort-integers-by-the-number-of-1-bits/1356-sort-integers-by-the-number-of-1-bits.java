d264a1d441409eb4375b4c68d77c82d183fcbce7
Shashwati Jha <141508757+Shashwati212324@users.noreply.github.com>
2026-02-25 08:50:04 +0530

    Time: 14 ms (20.86%), Space: 47 MB (27.94%) - LeetHub

--- 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.java
+++ 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.java
@@ -1,23 +1,24 @@
+import java.util.*;
+
 class Solution {
     public int[] sortByBits(int[] arr) {
-        Integer[] nums = Arrays.stream(arr).boxed().toArray(Integer[]::new);
+        int n = arr.length;
+        Integer[] nums = new Integer[n];
+        Map<Integer, Integer> bitCountMap = new HashMap<>();
+
+        for (int i = 0; i < n; i++) {
+            nums[i] = arr[i];
+            bitCountMap.put(arr[i], Integer.bitCount(arr[i]));
+        }
 
         Arrays.sort(nums, (a, b) -> {
-            int bitA = countOnes(a);
-            int bitB = countOnes(b);
+            int bitA = bitCountMap.get(a);
+            int bitB = bitCountMap.get(b);
             if (bitA != bitB) return bitA - bitB;
             return a - b;
         });
 
-        return Arrays.stream(nums).mapToInt(Integer::intValue).toArray();
-    }
-
-    private int countOnes(int n) {
-        int count = 0;
-        while (n != 0) {
-            n = n & (n - 1);
-            count++;
-        }
-        return count;
+        for (int i = 0; i < n; i++) arr[i] = nums[i];
+        return arr;
     }
 }
