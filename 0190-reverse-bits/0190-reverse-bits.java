5cf3127786cd3b898d0efa471b0a5e19d508c471
Shashwati Jha <141508757+Shashwati212324@users.noreply.github.com>
2026-03-03 21:49:23 +0530

    Time: 1 ms (62.65%), Space: 42.7 MB (17.12%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.java
+++ 0190-reverse-bits/0190-reverse-bits.java
@@ -1,22 +1,36 @@
 public class Solution {
-    // you need treat n as an unsigned value
-    public int reverseBits(int n) {
-        String str ="";
-        for(int i =0; i<32;i++){
-            if((n&(1<<i))!=0){
-                str+=1;
-            }
-            else{
-                str+=0;
-            }
-        }
-        return binarytoDecimal(str);
-    }
-    public int binarytoDecimal(String n) {
+   
+     public int reverseBits(int n) {
+
+         // you need treat n as an unsigned value
+    //-------------Brute force-----------------
+    //     String str ="";
+    //     for(int i =0; i<32;i++){
+    //         if((n&(1<<i))!=0){
+    //             str+=1;
+    //         }
+    //         else{
+    //             str+=0;
+    //         }
+    //     }
+    //     return binarytoDecimal(str);
+    // }
+    // public int binarytoDecimal(String n) {
+    //     int result = 0;
+    //     for (int i = 0; i < n.length(); i++) {
+    //         result = result * 2 + (n.charAt(i) - '0');
+    //     }
+    //     return result;
+
+        //optimized
         int result = 0;
-        for (int i = 0; i < n.length(); i++) {
-            result = result * 2 + (n.charAt(i) - '0');
+
+        for (int i = 0; i < 32; i++) {
+            result <<= 1;          // make space
+            result |= (n & 1);     // copy last bit of n
+            n >>>= 1;              // unsigned right shift
         }
+
         return result;
     }
 }
