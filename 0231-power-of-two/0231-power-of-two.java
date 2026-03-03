743baf00627b91ebf9847c6a06994100368e6444
Shashwati Jha <141508757+Shashwati212324@users.noreply.github.com>
2026-03-03 20:51:06 +0530

    Time: 0 ms (100%), Space: 42.6 MB (74.64%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.java
+++ 0231-power-of-two/0231-power-of-two.java
@@ -10,12 +10,17 @@ class Solution {
         //     return false;
         // }
         // return isPowerOfTwo(n/2);
-        if(n<=0){
-            return false;
-        }
-        while(n%2==0){
-            n=n/2;
-        }
-        return n==1;
+
+        //normal number system
+        // if(n<=0){
+        //     return false;
+        // }
+        // while(n%2==0){
+        //     n=n/2;
+        // }
+        // return n==1;
+
+        //bit manipulation
+        return n > 0 && (n & (n - 1)) == 0;
     }
 }
