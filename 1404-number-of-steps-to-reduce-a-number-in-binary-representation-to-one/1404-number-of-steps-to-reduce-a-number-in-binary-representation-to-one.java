a6384d7122b3607258667eab84bb59f23db00e0c
Shashwati Jha <141508757+Shashwati212324@users.noreply.github.com>
2026-03-05 10:53:11 +0530

    Time: 0 ms (100%), Space: 42.8 MB (51.78%) - LeetHub

--- 1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one/1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one.java
+++ 1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one/1404-number-of-steps-to-reduce-a-number-in-binary-representation-to-one.java
@@ -1,39 +1,28 @@
 class Solution {
     public int numSteps(String s) {
-        // Convert string to char array for easier manipulation
-        char[] arr = s.toCharArray();
         int steps = 0;
-        int i = arr.length - 1; // start from the least significant bit
+        int carry = 0;
 
-        while (i > 0) { // stop when only the first bit remains
-            if (arr[i] == '0') {
-                // Even: divide by 2 → just move left
-                i--;
-                steps++;
-            } else {
-                // Odd: add 1
-                steps++;
-                // propagate the carry
-                int j = i;
-                while (j >= 0 && arr[j] == '1') {
-                    arr[j] = '0';
-                    j--;
-                }
-                if (j >= 0) {
-                    arr[j] = '1';
-                } else {
-                    // carry goes beyond the first bit → prepend '1'
-                    char[] newArr = new char[arr.length + 1];
-                    newArr[0] = '1';
-                    for (int k = 0; k < arr.length; k++) {
-                        newArr[k + 1] = arr[k];
-                    }
-                    arr = newArr;
-                    i++; // adjust index because of new bit
-                }
+        // from LSB to just after MSB (i > 0)
+        for (int i = s.length() - 1; i > 0; i--) {
+            int bit = (s.charAt(i) - '0') + carry;
+
+            if (bit == 0) {
+                // even: divide by 2
+                steps += 1;
+            } else if (bit == 1) {
+                // odd: +1 then /2
+                steps += 2;
+                carry = 1;
+            } else { // bit == 2
+                // even: /2
+                steps += 1;
+                carry = 1;
             }
         }
 
-        return steps;
+        // If after processing all lower bits we still have carry,
+        // it means MSB became 2 -> needs one extra step.
+        return steps + carry;
     }
 }
