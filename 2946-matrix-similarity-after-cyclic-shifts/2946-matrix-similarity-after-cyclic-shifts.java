68a228be9713551853bf6f0446c45f24261f851b
Shashwati Jha <141508757+Shashwati212324@users.noreply.github.com>
2026-03-27 10:59:47 +0530

    Time: 2 ms (10.13%), Space: 47.1 MB (23.73%) - LeetHub

---
+++ 2946-matrix-similarity-after-cyclic-shifts/2946-matrix-similarity-after-cyclic-shifts.java
@@ -0,0 +1,53 @@
+class Solution {
+    public boolean areSimilar(int[][] mat, int k) {
+        int rows= mat.length;
+        int cols=mat[0].length;
+        k = k % cols;
+
+        // deep copy of original matrix
+        int[][] original = new int[rows][cols];
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                original[i][j] = mat[i][j];
+            }
+        }
+
+        for(int i =0; i<mat.length; i++){
+            if(i%2!=0){
+            for(int m =0; m<k;m++){
+                RightShift(mat,i);
+            }
+            }
+            else{
+               for(int m =0; m<k;m++){
+                   LeftShift(mat,i);
+                } 
+            }
+        }
+        // compare with original
+        for (int i = 0; i < mat.length; i++) {
+            for (int j = 0; j < mat[0].length; j++) {
+                if (mat[i][j] != original[i][j]) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+        
+    }
+    public void RightShift(int[][] mat, int i){
+        int l=mat[0].length, x =mat[i][l-1];
+        for(int j =l-1; j>0; j--){
+            mat[i][j]=mat[i][j-1];
+        }
+        mat[i][0]=x;
+    }
+    public void LeftShift(int[][] mat, int i){
+        int l =mat[0].length,x=mat[i][0];
+        for(int j =0; j<l-1 ;j++){
+            mat[i][j]=mat[i][j+1];
+        }
+        mat[i][l-1]=x;        
+    }
+}
