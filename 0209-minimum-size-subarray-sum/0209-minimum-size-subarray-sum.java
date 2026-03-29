dbfa7b6f1c18e59dbc376f1f289903cd7dfdab4e
Shashwati Jha <141508757+Shashwati212324@users.noreply.github.com>
2026-03-29 05:39:58 +0530

    Time: 12 ms (3.02%), Space: 61.2 MB (97.89%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.java
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.java
@@ -1,19 +1,88 @@
 class Solution {
     public int minSubArrayLen(int target, int[] nums) {
-        int len = Integer.MAX_VALUE;
-        int sum = 0;
-        int j = 0;
+       
+        int[] prefix = new int[nums.length + 1];
 
         for (int i = 0; i < nums.length; i++) {
-            sum += nums[i];
+            prefix[i + 1] = prefix[i] + nums[i];
+        }
 
-            while (sum >= target) {
-                len = Math.min(len, i - j + 1);
-                sum -= nums[j];
-                j++;
+        int len = Integer.MAX_VALUE;
+
+        for (int i = 0; i < nums.length; i++) {
+            int required = prefix[i] + target;
+            int j = lowerBound(prefix, required);
+
+            if (j != -1) {
+                len = Math.min(len, j - i);
             }
         }
 
         return len == Integer.MAX_VALUE ? 0 : len;
     }
+
+    private int lowerBound(int[] prefix, int target) {
+        int left = 0;
+        int right = prefix.length - 1;
+        int ans = -1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (prefix[mid] >= target) {
+                ans = mid;
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        return ans;
+    }
+
+
+        //OPTIMIZED O(n)
+        // int len = Integer.MAX_VALUE;
+        // int sum = 0;
+        // int j = 0;
+
+        // for (int i = 0; i < nums.length; i++) {
+        //     sum += nums[i];
+
+        //     while (sum >= target) {
+        //         len = Math.min(len, i - j + 1);
+        //         sum -= nums[j];
+        //         j++;
+        //     }
+        // }
+
+        // return len == Integer.MAX_VALUE ? 0 : len;
+
+        //BRUTE FORCE
+        
+    
+        //int len = Integer.MAX_VALUE;
+
+        // for (int i = 0; i < nums.length; i++) {
+        //     int j = i + 1;
+        //     int sum = nums[i];
+        //     int count = 1;
+
+        //     if (sum >= target) return 1;
+
+        //     while (sum < target && j < nums.length) {
+        //         sum += nums[j];
+        //         j++;
+        //         count++;
+        //     }
+
+        //     if (sum >= target) {
+        //         len = Math.min(len, count);
+        //     }
+        // }
+
+        // return len == Integer.MAX_VALUE ? 0 : len;
+    
+     
+    
 }
