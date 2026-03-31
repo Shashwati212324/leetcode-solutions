b62d7ecd717b4f33e37b6d1fa3658fff2b2e893e
Shashwati Jha <141508757+Shashwati212324@users.noreply.github.com>
2026-03-31 06:06:43 +0530

    Time: 68 ms (20.42%), Space: 48.1 MB (6.87%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
@@ -1,25 +1,58 @@
 class Solution {
     public List<Integer> findAnagrams(String s, String p) {
         List<Integer> list = new ArrayList<>();
-        int[] sFreq = new int[26];
-        int[] pFreq = new int[26];
+        if (p.length() > s.length()) return list;
 
-        for(char ch:p.toCharArray()){
-            pFreq[ch-'a']++;
-        }
-        int left =0;
-        int k=p.length();
-        for(int i =0; i<s.length();i++){
-            sFreq[s.charAt(i)-'a']++;
+        HashMap<Character, Integer> map1 = new HashMap<>();
+        HashMap<Character, Integer> map2 = new HashMap<>();
 
-            if(i-left+1>k){
-                sFreq[s.charAt(left)-'a']--;
-                left++;
+        for (int i = 0; i < p.length(); i++) {
+            map1.put(p.charAt(i), map1.getOrDefault(p.charAt(i), 0) + 1);
+        }
+
+        int i = 0;
+        for (int j = 0; j < s.length(); j++) {
+            char ch = s.charAt(j);
+            map2.put(ch, map2.getOrDefault(ch, 0) + 1);
+
+            if (j - i + 1 > p.length()) {
+                char leftChar = s.charAt(i);
+                map2.put(leftChar, map2.get(leftChar) - 1);
+                if (map2.get(leftChar) == 0) {
+                    map2.remove(leftChar);
+                }
+                i++;
             }
-            if(i-left+1 ==k && Arrays.equals(pFreq,sFreq)){
-                list.add(left);
+
+            if (j - i + 1 == p.length() && map1.equals(map2)) {
+                list.add(i);
             }
         }
+
         return list;
     }
 }
+
+        // List<Integer> list = new ArrayList<>();
+        // int[] sFreq = new int[26];
+        // int[] pFreq = new int[26];
+
+        // for(char ch:p.toCharArray()){
+        //     pFreq[ch-'a']++;
+        // }
+        // int left =0;
+        // int k=p.length();
+        // for(int i =0; i<s.length();i++){
+        //     sFreq[s.charAt(i)-'a']++;
+
+        //     if(i-left+1>k){
+        //         sFreq[s.charAt(left)-'a']--;
+        //         left++;
+        //     }
+        //     if(i-left+1 ==k && Arrays.equals(pFreq,sFreq)){
+        //         list.add(left);
+        //     }
+        // }
+        // return list;
+
+
