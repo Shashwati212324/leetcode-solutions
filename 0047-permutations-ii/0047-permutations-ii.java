9a7441ea94e42b779d6eb41a4c0e65beff7a21b8
Shashwati Jha <141508757+Shashwati212324@users.noreply.github.com>
2026-04-01 11:51:42 +0530

    Time: 3 ms (89.5%), Space: 46.6 MB (83.77%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.java
+++ 0047-permutations-ii/0047-permutations-ii.java
@@ -1,24 +1,29 @@
 class Solution {
     public List<List<Integer>> permuteUnique(int[] nums) {
-        List<List<Integer>> list = new ArrayList<>();
         Arrays.sort(nums);
-        boolean[] visited = new boolean[nums.length];
-        backtrack(list,new ArrayList<>(),visited, nums);
-        return list;
+        ArrayList<Integer> list = new ArrayList<>();
+        List<List<Integer>> answer = new ArrayList<>();
+        boolean[] used= new boolean[nums.length];
+
+        backtrackDuplicates(used, nums, list, answer);
+        return answer;
+       
     }
-    public void backtrack(List<List<Integer>> list, List<Integer> temp, boolean[] visited, int[] nums){
-        if(temp.size()==nums.length){
-            list.add(new ArrayList<>(temp));
+    public void backtrackDuplicates(boolean[] used, int[] nums, ArrayList<Integer> list, List<List<Integer>> answer){
+        if(list.size() == nums.length){
+            answer.add(new ArrayList<>(list));
             return;
         }
-        for(int i =0; i<nums.length ;i++){
-            if(visited[i] == true)continue;
-            if(i>0 && nums[i]==nums[i-1] && !visited[i-1])continue;
-            visited[i]=true;
-            temp.add(nums[i]);
-            backtrack(list,temp,visited,nums);
-            temp.remove(temp.size()-1);
-            visited[i]=false;
+
+        for(int i=0; i<nums.length; i++){
+            if(used[i])continue;
+            if(i>0 && nums[i]==nums[i-1] && !used[i-1])continue;
+            
+            used[i]=true;
+            list.add(nums[i]);
+            backtrackDuplicates(used, nums, list ,answer);
+            used[i]=false;
+            list.remove(list.size()-1);
         }
     }
 }
