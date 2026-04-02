022b233df6d3b07f47a71b4920b61c712d5bbe37
Shashwati Jha <141508757+Shashwati212324@users.noreply.github.com>
2026-04-03 05:19:03 +0530

    Time: 5 ms (99.56%), Space: 51.3 MB (5.19%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.java
+++ 0040-combination-sum-ii/0040-combination-sum-ii.java
@@ -1,22 +1,25 @@
 class Solution {
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
-        List<List<Integer>> list = new ArrayList<>();
-        Arrays.sort(candidates);
-        TargetList(candidates, 0,target,new ArrayList<>(), list);
-        return list;
-        
+       Arrays.sort(candidates);
+       ArrayList<Integer> list = new ArrayList<>();
+       List<List<Integer>> answer = new ArrayList<>();
+       backtrack(list, answer, candidates, 0, 0, target);
+       return answer;
     }
-    public static void TargetList(int[] nums,int start, int target, List<Integer> temp, List<List<Integer>> arr){
-        if(target==0){
-            arr.add(new ArrayList<>(temp));
+    public void backtrack(ArrayList<Integer> list,List<List<Integer>> answer,int[] nums, int sum, int start,int target){
+        if(sum == target){
+            answer.add(new ArrayList<>(list));
             return;
         }
-        if(target<0)return;
-        for(int i=start; i<nums.length;i++){
-            if(i>start && nums[i]==nums[i-1]) continue;
-            temp.add(nums[i]);
-            TargetList(nums,i+1, target-nums[i],temp,arr);
-            temp.remove(temp.size()-1);
+        //if(start > nums.length && sum != target)return;
+        for(int i = start; i<nums.length; i++){
+            if(sum+nums[i]>target)return;
+            
+            if(i>start && nums[i]==nums[i-1])continue;
+            list.add(nums[i]);
+            backtrack(list, answer, nums, sum+nums[i], i+1, target);
+            list.remove(list.size()-1);
+            
         }
     }
     
