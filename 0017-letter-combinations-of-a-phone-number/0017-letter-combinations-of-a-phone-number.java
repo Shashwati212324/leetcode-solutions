0f07a72709eaf6787f367ddb3a6d1269bddbb16a
Shashwati Jha <141508757+Shashwati212324@users.noreply.github.com>
2026-04-04 11:37:49 +0530

    Time: 0 ms (100%), Space: 43.6 MB (59.66%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -1,31 +1,24 @@
 class Solution {
     public List<String> letterCombinations(String digits) {
-        List<String> result = new ArrayList<>();
-
-        if (digits.length() == 0) return result;
-
-        String[] arr = {
-            "", "", "abc", "def", "ghi",
-            "jkl", "mno", "pqrs", "tuv", "wxyz"
-        };
-
-        backtrack(0, new StringBuilder(), digits, arr, result);
-        return result;
+        String[] array = {"","", "abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+        List<String> list = new ArrayList<>();
+        StringBuilder sb = new StringBuilder();
+        backtrack(array, sb, list, digits);
+        return list;
     }
-
-    private void backtrack(int index, StringBuilder sb, String digits, String[] arr, List<String> result) {
-        if (index == digits.length()) {
-            result.add(sb.toString());
+    public void backtrack(String[] ar, StringBuilder sb, List<String> list, String digits){
+        if(sb.length() == digits.length()){
+            list.add(sb.toString());
             return;
         }
+        int index= sb.length();
+        int digit = digits.charAt(index)-'0';
+        String s = ar[digit];
 
-        int digit = digits.charAt(index) - '0';
-        String letters = arr[digit];
-
-        for (char ch : letters.toCharArray()) {
+        for(char ch: s.toCharArray()){
             sb.append(ch);
-            backtrack(index + 1, sb, digits, arr, result);
-            sb.deleteCharAt(sb.length() - 1);  // backtrack
+            backtrack(ar,sb,list,digits);
+            sb.deleteCharAt(sb.length()-1);
         }
     }
 }
