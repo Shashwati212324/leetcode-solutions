34c924f30c39f5873f9b2cfe4c325bfec2e89b2e
Shashwati Jha <141508757+Shashwati212324@users.noreply.github.com>
2026-04-11 21:26:00 +0530

    Time: 3 ms (26.53%), Space: 65.2 MB (68.9%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -3,24 +3,43 @@ class Solution {
         int n = nums.length;
         int[] answer = new int[n];
 
-        // Step 1: prefix products
+        // // Step 1: prefix products
+        // int[] prefix = new int[n];
+        // prefix[0] = 1;
+        // for (int i = 1; i < n; i++) {
+        //     prefix[i] = prefix[i - 1] * nums[i - 1];
+        // }
+
+        // // Step 2: suffix products
+        // int[] suffix = new int[n];
+        // suffix[n - 1] = 1;
+        // for (int i = n - 2; i >= 0; i--) {
+        //     suffix[i] = suffix[i + 1] * nums[i + 1];
+        // }
+
+        // // Step 3: multiply prefix and suffix
+        // for (int i = 0; i < n; i++) {
+        //     answer[i] = prefix[i] * suffix[i];
+        // }
+
         int[] prefix = new int[n];
-        prefix[0] = 1;
-        for (int i = 1; i < n; i++) {
-            prefix[i] = prefix[i - 1] * nums[i - 1];
-        }
-
-        // Step 2: suffix products
         int[] suffix = new int[n];
-        suffix[n - 1] = 1;
-        for (int i = n - 2; i >= 0; i--) {
-            suffix[i] = suffix[i + 1] * nums[i + 1];
+
+        prefix[0] = 1;
+        for(int i =1; i<n;i++){
+            prefix[i] = prefix[i-1]*nums[i-1];
+
+        }
+        suffix[n-1] = 1;
+        for(int i =n-2; i>=0;i--){
+            suffix[i] =suffix[i+1]*nums[i+1];
+
         }
 
-        // Step 3: multiply prefix and suffix
-        for (int i = 0; i < n; i++) {
+        for(int i =0; i<n;i++){
             answer[i] = prefix[i] * suffix[i];
         }
+
 
         return answer;
     }
