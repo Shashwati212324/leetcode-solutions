43d0a15d947149605d361b241883011d6cab2135
Shashwati Jha <141508757+Shashwati212324@users.noreply.github.com>
2026-04-18 21:52:17 +0530

    Time: 2 ms (99.28%), Space: 47.5 MB (13.52%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,5 +1,7 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
+
+        //TCS REVISION
         HashMap<Integer, Integer> map = new HashMap<>();
         for(int i=0; i<nums.length; i++){
             int x = target-nums[i];
