665f59675550c8f7b6c22df6f4a7d4b89cf7a729
Shashwati Jha <141508757+Shashwati212324@users.noreply.github.com>
2026-06-19 09:01:23 +0530

    Time: 18 ms (41.79%), Space: 50.3 MB (96.29%) - LeetHub

--- 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.java
+++ 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.java
@@ -1,13 +1,62 @@
+// BASIC
+// class Solution {
+//     public int kthSmallest(int[][] matrix, int k) {
+//         PriorityQueue<Integer> pq = new PriorityQueue<>();
+//         // for(int i =0;i<matrix.length; i++){
+//         //     for(int num : matrix[i]){
+//         //         pq.offer(num);
+//         //     }
+//         // }
+
+//         // for(int i = 1; i<k;i++)pq.poll();
+//         // return pq.peek();
+
+//     }    
+// }
+
 class Solution {
+
     public int kthSmallest(int[][] matrix, int k) {
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
-        for(int i =0;i<matrix.length; i++){
-            for(int num : matrix[i]){
-                pq.offer(num);
-            }
+
+        int n = matrix.length;
+
+        PriorityQueue<int[]> pq =
+            new PriorityQueue<>(
+                (a,b) -> a[0] - b[0]
+            );
+
+        for(int row = 0; row < n; row++) {
+
+            pq.offer(
+                new int[]{
+                    matrix[row][0],
+                    row,
+                    0
+                }
+            );
         }
 
-        for(int i = 1; i<k;i++)pq.poll();
-        return pq.peek();
+        while(k > 1) {
+
+            int[] curr = pq.poll();
+
+            int row = curr[1];
+            int col = curr[2];
+
+            if(col + 1 < n) {
+
+                pq.offer(
+                    new int[]{
+                        matrix[row][col + 1],
+                        row,
+                        col + 1
+                    }
+                );
+            }
+
+            k--;
+        }
+
+        return pq.peek()[0];
     }
 }
